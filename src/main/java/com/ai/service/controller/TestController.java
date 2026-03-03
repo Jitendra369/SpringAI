1a3cb3236c1aa2312cdde1f6fa46f21e809b0a89
jitendra <onekb.twotb@gmail.com>
2026-03-03 17:33:32 +0530

    fix api

--- src/main/java/com/ai/service/controller/TestController.java
+++ src/main/java/com/ai/service/controller/TestController.java
@@ -2,8 +2,10 @@ package com.ai.service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/test")
+@RestController
 public class TestController {
 
 
