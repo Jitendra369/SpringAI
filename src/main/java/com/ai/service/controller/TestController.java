804546209e531cbe68c7cab14c0e559ba90f0dc5
jitendra <onekb.twotb@gmail.com>
2026-03-03 18:35:50 +0530

    push

--- src/main/java/com/ai/service/controller/TestController.java
+++ src/main/java/com/ai/service/controller/TestController.java
@@ -11,6 +11,6 @@ public class TestController {
 
     @GetMapping
     public String testApi(){
-        return "Deployment using jenking working Fine  after adding webhook";
+        return "Deployment using jenking working Fine  after adding webhook 1";
     }
 }
